735e363cb56a91b9d3e99b53090b1e678602c3f3
HotPotatoC <juandotulung@gmail.com>
2023-08-03 19:58:10 +0700

    fix(login): footer links

--- components/screens/home/login-footer.tsx
+++ components/screens/home/login-footer.tsx
@@ -1,22 +1,40 @@
 export const LoginFooter = () => {
   const links = [
     { href: 'https://about.twitter.com', label: 'About' },
-    { href: 'https://help.twitter.com', label: 'Help' },
+    { href: 'https://help.twitter.com', label: 'Help Center' },
     { href: 'https://twitter.com/tos', label: 'Terms of Service' },
-    { href: 'https://twitter.com/tos', label: 'Privacy Policy' },
-    { href: 'https://twitter.com/tos', label: 'Cookie Policy' },
-    { href: 'https://twitter.com/tos', label: 'Accessibility' },
-    { href: 'https://twitter.com/tos', label: 'Ads info' },
-    { href: 'https://twitter.com/tos', label: 'Blog' },
-    { href: 'https://twitter.com/tos', label: 'Status' },
-    { href: 'https://twitter.com/tos', label: 'Careers' },
-    { href: 'https://twitter.com/tos', label: 'Brand Resources' },
-    { href: 'https://twitter.com/tos', label: 'Advertising' },
-    { href: 'https://twitter.com/tos', label: 'Marketing' },
-    { href: 'https://twitter.com/tos', label: 'Twitter for Business' },
-    { href: 'https://twitter.com/tos', label: 'Developers' },
-    { href: 'https://twitter.com/tos', label: 'Directory' },
-    { href: 'https://twitter.com/tos', label: 'Settings' },
+    { href: 'https://twitter.com/privacy', label: 'Privacy Policy' },
+    {
+      href: 'https://support.twitter.com/articles/20170514',
+      label: 'Cookie Policy',
+    },
+    {
+      href: 'https://help.twitter.com/resources/accessibility',
+      label: 'Accessibility',
+    },
+    {
+      href: 'https://business.twitter.com/en/help/troubleshooting/how-twitter-ads-work.html?ref=web-twc-ao-gbl-adsinfo&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=adsinfo',
+      label: 'Ads info',
+    },
+    { href: 'https://blog.twitter.com/', label: 'Blog' },
+    { href: 'https://status.twitterstat.us/', label: 'Status' },
+    { href: 'https://careers.twitter.com/', label: 'Careers' },
+    {
+      href: 'https://about.twitter.com/press/brand-assets',
+      label: 'Brand Resources',
+    },
+    {
+      href: 'https://ads.twitter.com/?ref=gl-tw-tw-twitter-advertise',
+      label: 'Advertising',
+    },
+    { href: 'https://marketing.twitter.com/', label: 'Marketing' },
+    {
+      href: 'https://business.twitter.com/?ref=web-twc-ao-gbl-twitterforbusiness&utm_source=twc&utm_medium=web&utm_campaign=ao&utm_content=twitterforbusiness',
+      label: 'Twitter for Business',
+    },
+    { href: 'https://developer.twitter.com/', label: 'Developers' },
+    { href: 'https://twitter.com/i/directory/profiles', label: 'Directory' },
+    { href: 'https://twitter.com/settings', label: 'Settings' },
   ]
 
   return (
